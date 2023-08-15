fe31635aa3b846c76b277d057bae1bdccf7b9eac
Zachary Kuchar <zacharyk323@live.com>
2023-08-15 19:50:18 -0400

    Display complete list of cat information

--- views/CatIndex.jsx
+++ views/CatIndex.jsx
@@ -6,7 +6,16 @@ function CatIndex(props) {
       <h1>Browse the list of cats</h1>
       {
         props.cats.map((cat)=>{
-            <h1>{cat.name}</h1>
+            return <div>
+                <h1>{cat.name}</h1>
+                <h2>{cat.age}</h2>
+                {
+                    cat.hadFirstCheckup ? 
+                    <h3>Had a checkup</h3> : 
+                    <h3>Needs a checkup</h3>
+                }
+                <h3>{cat.description}</h3>
+            </div>
         })
       }
     </div>
