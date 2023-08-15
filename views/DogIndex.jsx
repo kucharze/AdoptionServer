437caa0cc6acf7c5ac4556aabc6df1d9e5326c9a
Zachary Kuchar <zacharyk323@live.com>
2023-08-15 18:38:38 -0400

    Link to index

--- views/DogIndex.jsx
+++ views/DogIndex.jsx
@@ -9,6 +9,7 @@ function DogIndex(props) {
             return (<div>
                 <h1>{dog.name}</h1>
                 <h2>{dog.breed}</h2>
+                <a href={`/dogs/${dog._id}/edit`}>Edit this tag</a>
                 <form action={`/dogs/${dog._id}?_method=DELETE`} 
                     method="POST">
                         <input type="submit" value="DELETE"/>
