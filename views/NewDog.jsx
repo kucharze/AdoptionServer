ea8feb9d744dca9802d376a4a7370e74ac71cff1
Zachary Kuchar <zacharyk323@live.com>
2023-08-15 12:16:19 -0400

    New dog form

---
+++ views/NewDog.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function NewDog() {
+  return (
+    <div>
+        <form action="/dogs" method="POST">
+            Name: <input type="text" name="name" /><br/>
+            Age: <input type="number" name="age" /><br/>
+            Description: <input type="text" name="description" /><br/>
+            Breed: <input type="text" name="breed" /><br/>
+            <input type="submit" name="" value="Create Fruit"/>
+        </form>
+    </div>
+  )
+}
+
+export default NewDog
